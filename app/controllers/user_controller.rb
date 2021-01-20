154079e96b0872bcd1ceae5f3ee4d554046de684
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-20 16:53:11 +0530

    Add name validations on User

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,19 @@
 class UserController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      redirect_to root
+    else
+      render :new
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
