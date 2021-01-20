16276f28487328005409f4d68f7258203cb488b9
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-20 19:38:14 +0530

    User will get not automatically logged in

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,6 @@ class UserController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      session[:user_id] = @user.id
       redirect_to root_url
     else
       render :new
