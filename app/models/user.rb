42e9ecad79e81a152e7f48d247033db8dad5f0ef
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-20 16:28:30 +0530

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
