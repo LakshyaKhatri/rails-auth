0832f0acecbf2168820f52f4203a397b673065a4
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-20 17:51:46 +0530

    Add validations for password confirmation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,13 +7,21 @@ class UserController < ApplicationController
     user = User.new(user_params)
 
     if user.save
-      redirect_to root
+      redirect_to root_url
+      session[:user_id] = user.id
     else
       render :new
+    end
   end
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :first_name, presence: true, length: { maximum: 30 }
+  validates :last_name, presence: true, length: { maximum: 30 }
+  validates :password, confirmation: { case_sensitive: true }
 end
