c2290a55b2ca30efee749a78af6980cea4b1f934
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-20 21:02:05 +0530

    Add mailer configurations

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,17 @@ module Auth
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.action_mailer.default_url_options = { host: 'localhost:3000' }
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'temp',
+      password:             'temp',
+      authentication:       'plain'
+    }
 
     # Configuration for the application, engines, and railties goes here.
     #
