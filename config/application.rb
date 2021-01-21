3d93e850d299e534a8211750bdba1a042f261ae3
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-21 12:00:05 +0530

    Add mailer configurations

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Auth
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    # Mailer configurations
     config.action_mailer.default_url_options = { host: 'localhost:3000' }
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
