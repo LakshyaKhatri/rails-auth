abd6ffa85f40ec8d9737536ef1b5405a11e4c768
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-22 17:23:50 +0530

    Add DB constraints for user model

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,15 @@
 ActiveRecord::Schema.define(version: 2021_01_21_063903) do
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password_digest"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
