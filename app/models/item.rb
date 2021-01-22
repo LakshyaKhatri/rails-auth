f3b12f8b5626e7c24436ccdcb21e5306fcf1315b
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-22 19:32:45 +0530

    Fix: dependent field is incorrect

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :category, dependent: :restrict_with_error
+  belongs_to :category
 
   has_many :applied_taxes
   has_many :taxes, through: :applied_taxes
