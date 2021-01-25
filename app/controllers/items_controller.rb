ad44b3aa1f4841570783471a6594da4ad4e06008
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-25 17:43:57 +0530

    Add controller to create new item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,21 @@
 class ItemsController < ApplicationController
+  before_action :require_login
+
   def new
     @item = Item.new(category_id: 1)
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to admin_dashboard_url
+    else
+      render :new
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price, :in_stock, :is_imported, :category_id)
   end
 end
