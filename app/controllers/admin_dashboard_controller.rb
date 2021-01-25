fb2ff78c5d73bf1154db9c542f31eee6dfc7abe2
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-25 13:06:31 +0530

    Made Admin Dashboard View to show currently available things

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -2,5 +2,8 @@ class AdminDashboardController < ApplicationController
   before_action :require_login
 
   def index
+    @item_count = Item.count()
+    @category_count = Category.count()
+    @types_of_tax_count = Tax.count()
   end
 end
