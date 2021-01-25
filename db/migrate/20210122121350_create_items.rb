39d6ec8bb1d8adf57d22b8bd5d6f6477fe8805f5
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-25 17:33:22 +0530

    Add field :in_stock for Items

--- db/migrate/20210122121350_create_items.rb
+++ db/migrate/20210122121350_create_items.rb
@@ -3,6 +3,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
     create_table :items do |t|
       t.string :name, null: false
       t.float :price, null: false
+      t.integer :in_stock, null: false
       t.boolean :is_imported, null: false
       t.references :category, foreignkey: true, default: 1
 
--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_01_22_124516) do
   create_table "items", force: :cascade do |t|
     t.string "name", null: false
     t.float "price", null: false
+    t.integer "in_stock", null: false
     t.boolean "is_imported", null: false
     t.integer "category_id", default: 1
     t.datetime "created_at", precision: 6, null: false
