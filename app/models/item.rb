3a4b2264342c84d1d93b1780cefafc8014030987
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-25 19:23:47 +0530

    Modify Item validation for categories

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,11 @@ class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.1 }
   validates :in_stock, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validate :category_exists
+
+  def category_exists
+    unless Category.find_by(id: self.category_id).present?
+      errors.add(:category_id, "category doesn't exists")
+    end
+  end
 end
