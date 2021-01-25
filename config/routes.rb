c99a75ae51f9b22c84814062ca79bff8deb89669
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-25 13:47:16 +0530

    Move Forgot Password route to Admin Forgot password

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,23 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   root 'admin_dashboard#index'
 
-  get '/signup', to: 'registration#new'
-  post '/signup', to: 'registration#create'
-
-  scope '/password', as: :password do
-    get '/forgot', to: 'password#forgot'
-    post '/forgot', to: 'password#create_reset_token'
-    get '/reset/:token', to: 'password#reset', as: :reset
-    post '/reset/:token', to: 'password#change_password', as: :change_password
-  end
-
   scope '/admin', as: :admin do
     get '/dashboard', to: 'admin_dashboard#index'
 
     get '/login', to: 'session#login'
     post '/login', to: 'session#new'
     get '/logout', to: 'session#logout'
+
+    get '/signup', to: 'registration#new'
+    post '/signup', to: 'registration#create'
     # resources :items, only: [:new, :create]
+
+    scope '/password', as: :password do
+      get '/forgot', to: 'password#forgot'
+      post '/forgot', to: 'password#create_reset_token'
+      get '/reset/:token', to: 'password#reset', as: :reset
+      post '/reset/:token', to: 'password#change_password', as: :change_password
+    end
   end
 
   mount Sidekiq::Web => '/sidekiq'
