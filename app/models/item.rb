15a3e578060ceb397c36eca2a5f2582d03b173c4
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-27 12:10:07 +0530

    Add methods to calculate tax

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,4 +14,13 @@ class Item < ApplicationRecord
       errors.add(:category_id, "category doesn't exists")
     end
   end
+
+  def calculate_tax
+    total_tax_rate = 0.0
+    self.taxes.each {|tax| total_tax_rate += tax.rate }
+
+    self.taxed_price = self.price
+    self.taxed_price += ((self.price * total_tax_rate) / 100.0)
+    self.taxed_price = self.taxed_price.round(2)
+  end
 end
