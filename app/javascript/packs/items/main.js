ad8cc3347af7e638cb4223411bbf2b989d343a4a
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-27 18:36:11 +0530

    Add js for populating items table

--- app/javascript/packs/items/main.js
+++ app/javascript/packs/items/main.js
@@ -1,3 +1,32 @@
-$('.add-to-cart-btn').click(function() {
-  console.log('it works');
+const BASE_URL = 'http://127.0.0.1:3000/';
+
+// Fetches items for given category and loads them using populate_table
+function loadItems(category){
+  $.get(BASE_URL + 'api/v1/items?category=' + category, function(data){
+    populateTable(data);
+  });
+}
+
+// takes the input items data and creates table rows for them
+function populateTable(items){
+  const importTag = '<p class="import-tag">Imported</p>';
+  const addBtn = '<button type="button" class="add-to-cart-btn">Add</button>';
+
+  const itemsTableBody = document.getElementsByClassName('items-table')[0].children[1];
+  itemsTableBody.innerHTML = ''
+
+  items.forEach((item, index) => {
+    const currentRow = itemsTableBody.insertRow(-1);
+
+    currentRow.insertCell(-1).innerHTML = item.name;
+    currentRow.insertCell(-1).innerHTML = item.taxed_price;
+    currentRow.insertCell(-1).innerHTML = item.is_imported ? importTag : '';
+    currentRow.insertCell(-1).innerHTML = addBtn;
+  });
+}
+
+// Loads items for the very first time
+loadItems(1);
+$('#item-category-select').change(function() {
+  loadItems(this.value);
 });
