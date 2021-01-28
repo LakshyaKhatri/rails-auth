42a829eaba42f074b6dfda6737064be002f8bd50
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-28 19:48:36 +0530

    Made utility methods private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
 
+  private
   def current_user
     User.find_by(id: session[:user_id])
   end
@@ -10,7 +11,6 @@ class ApplicationController < ActionController::Base
     current_user.present?
   end
 
-  private
   def require_login
     redirect_to admin_login_url unless logged_in?
   end
