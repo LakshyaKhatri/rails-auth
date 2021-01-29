b050477a3c5cad90a44a0b06338efa9207d507b0
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-29 18:28:22 +0530

    Add order creation action

---
+++ app/controllers/api/v1/order_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  module V1
+    class OrderController < Api::V1::ApiController
+      #TODO: use filters to check cart presence everywhere
+      def create
+        cart = current_cart
+        order_total = cart.cart_items.joins(:item).sum("items.taxed_price * cart_items.qty")
+        order = Order.new(cart: cart, total: order_total)
+
+        render_record_invalid order.errors unless order.save
+      end
+    end
+  end
+end
