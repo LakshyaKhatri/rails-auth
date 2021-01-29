1a9cc447bbdb62d8a8ac44bc78243f7e674748b6
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-29 19:03:41 +0530

    Fix routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   end
   get '/shop', to: 'shop#index'
   get '/cart', to: 'cart#index'
-  post '/checkout', to: 'order#index'
+  get '/checkout', to: 'order#index'
 
   # External appplication routes
   mount Sidekiq::Web => '/sidekiq'
