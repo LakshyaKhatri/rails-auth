3c1a164211fd4b9d52c2bc8c67e9b2441a30e51b
LakshyaKhatri <lakshyak67@gmail.com>
2021-01-29 13:31:21 +0530

    Remove item from cart if less than 1

--- app/javascript/packs/cart/main.js
+++ app/javascript/packs/cart/main.js
@@ -3,12 +3,19 @@ const BASE_URL = 'http://127.0.0.1:3000/api/v1/';
 $('.decrease-qty-btn').click(function() {
   const btn = $(this);
   const url = `${BASE_URL}/cart/decrease-qty/`;
+
   $.post(url,
     {
       cart_item_id: btn.parent().next().text(),
     },
     function(){
       btn.next().text(parseInt(btn.next().text()) - 1);
+    }
+  ).fail(function(){
+      if (confirm('Remove this item form cart?')) {
+        removeItemFromCart(btn.parent().next().next().text());
+        btn.parents('tr').remove();
+      }
     });
 });
 
@@ -23,3 +30,9 @@ $('.increase-qty-btn').click(function() {
       btn.prev().text(parseInt(btn.prev().text()) + 1);
     });
 });
+
+//TODO: Repeating, Could be moved to single file
+function removeItemFromCart(itemID){
+  const url = `${BASE_URL}cart/remove-item/`
+  $.post(url, { item_id: itemID, });
+}
