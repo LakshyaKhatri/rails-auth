77a0a71eb2b8a0ef74c5061fb0272455abffef4b
Lakshya Khatri <lakshyak67@gmail.com>
2021-01-29 07:53:14 +0530

    Add empty cart method

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,10 @@
 class Cart < ApplicationRecord
   has_many :cart_items
   has_many :items, through: :cart_items
+
+
+  private
+  def empty_cart
+    CartItem.where(cart:self).delete_all
+  end
 end
