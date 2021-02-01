3781a9aa1d155075b777206de27865eca4313d78
LakshyaKhatri <lakshyak67@gmail.com>
2021-02-01 14:12:31 +0530

    use simpler query to make it efficient

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,19 +1,11 @@
 module Api
   module V1
     class ItemsController < Api::V1::ApiController
-
       def list
-        #TODO: don't go insane, replace with rails 
-        @items = Item.find_by_sql(
-          "SELECT id, name, taxed_price, is_imported, in_stock, (CASE WHEN EXISTS (SELECT id FROM #{CartItem.table_name}
-            WHERE #{CartItem.table_name}.item_id = #{Item.table_name}.id AND
-            #{CartItem.table_name}.cart_id = #{session[:cart_id] || -1}
-          ) THEN 1 ELSE 0 END) as `in_cart`
-          FROM #{Item.table_name}
-          WHERE #{Item.table_name}.category_id = #{params[:category]} AND
-          #{Item.table_name}.in_stock >= 1
-          ORDER BY #{Item.table_name}.name"
-        )
+        @items = Item.left_outer_joins(:cart_items).where(
+          'items.category_id = ?', params[:category]
+        ).distinct.select("items.*, cart_items.id as cart_item_id").group('items.id')
+
         render json: @items
       end
     end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
 
   has_many :applied_taxes
   has_many :taxes, through: :applied_taxes
+  has_many :cart_items
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.1 }
