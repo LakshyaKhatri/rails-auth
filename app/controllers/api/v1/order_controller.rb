0dad40c85381bfbfaa16b68505b40a57747b7efb
Lakshya Khatri <lakshyak67@gmail.com>
2021-02-02 00:13:42 +0530

    Add order item controller

--- app/controllers/api/v1/order_controller.rb
+++ app/controllers/api/v1/order_controller.rb
@@ -2,12 +2,13 @@ module Api
   module V1
     class OrderController < Api::V1::ApiController
       def create
-        @cart = current_cart
-        return render_record_invalid "Order already present." if Order.find_by(cart: @cart).present?
+        cart = current_cart
 
-        order_total = @cart.cart_items.joins(:item).sum("items.taxed_price * cart_items.qty")
-        order = Order.new(cart: @cart, total: order_total)
-        render_record_invalid order.errors unless order.save
+        order_total = cart.cart_items.joins(:item).sum("items.taxed_price * cart_items.qty")
+        @order = Order.new(total: order_total)
+        return render_record_invalid order.errors unless @order.save
+
+        render json: @order
       end
     end
   end
